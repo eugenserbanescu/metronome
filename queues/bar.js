ec7e5f3e5efd48e86911897e8c58c24a18dc992e
Serbanescu <eugen.serbanescu@hmhco.com>
2017-10-13 18:12:07 +0100

    feat: added queue rotation

--- queues/bar.js
+++ queues/bar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
 import { notesList } from '../player/notes';
@@ -9,14 +10,25 @@ import styles from './styles';
 
 class Bar extends Component {
   state = {
-    isRotated: false
+    isRotated: true
+  }
+
+  rotate = () => {
+    this.setState({
+      isRotated: !this.state.isRotated
+    });
   }
 
   render() {
     const list = this.state.isRotated ? notesList.slice(0).reverse() : notesList;
     return (
-      <View style={this.state.isRotated ? styles.barRotated : styles.bar}>
-        {list.map(note => <Line isRotated={this.state.isRotated} key={`line-${note}`} note={note} />)}
+      <View style={styles.barContainer}>
+        <TouchableHighlight style={styles.rotateButton} onPress={this.rotate}>
+          <Text style={styles.rotateButtonText}>Rotate queues!</Text>
+        </TouchableHighlight>
+        <View style={this.state.isRotated ? styles.barRotated : styles.bar}>
+          {list.map(note => <Line isRotated={this.state.isRotated} key={`line-${note}`} note={note} />)}
+        </View>
       </View>
     )
   }
--- queues/styles.js
+++ queues/styles.js
@@ -1,9 +1,8 @@
 import { StyleSheet } from 'react-native';
 
 const bar = {
-  backgroundColor: '#ffffff',
+
   flexDirection: 'column',
-  padding: 20,
   alignItems: 'stretch',
   justifyContent: 'space-between',
 };
@@ -18,17 +17,32 @@ const note = {
   backgroundColor: '#b2d3e6',
   borderRadius: 4,
   flex: 1,
+  margin: 1,
   minHeight: 20,
   minWidth: 20,
   overflow: 'hidden'
 }
 
 const styles = StyleSheet.create({
+  barContainer: {
+    backgroundColor: '#ffffff',
+    padding: 20,
+  },
+  rotateButton: {
+    backgroundColor: '#cccccc',
+    borderRadius: 5,
+    width: 200
+  },
+  rotateButtonText: {
+    color: '#ffffff',
+    fontSize: 24,
+    textAlign: 'center'
+  },
   bar: bar,
   barRotated: {
     ...bar,
     flexDirection: 'row',
-    height: '100%',
+    height: '90%',
   },
   line: line,
   lineRotated: {
